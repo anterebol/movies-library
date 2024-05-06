8438fdf14f5086bbacfe703ef85f18109813efdb
Aleks <anterebol@gmail.com>
2024-05-06 23:00:57 +0300

    feat: add initial state into sort form

--- src/app/components/FormSorted/FormSorted.tsx
+++ src/app/components/FormSorted/FormSorted.tsx
@@ -4,8 +4,11 @@ export default function FormSorted() {
   const form = useForm({
     mode: 'uncontrolled',
     initialValues: {
-      name: '',
-      email: '',
+      genre: '',
+      release_year: '',
+      rating_from: '',
+      rating_to: '', 
+      sort_by: 'Most popular'
     },
   });
 }
