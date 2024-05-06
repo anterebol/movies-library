ec14f5c9f9cbd07f2237b5e4dc7b97311418d220
Aleks <anterebol@gmail.com>
2024-05-06 16:27:22 +0300

    feat: upgrade next config by redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const host = process.env.NEXT_PUBLIC_API_URL;
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/movies',
+        permanent: false
+      },
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/search_movies/:params',
+        destination: `${host}/3/discover/movie?language=en-US&:params`,
+      },
+    ];
+  }
+};
 
 export default nextConfig;
