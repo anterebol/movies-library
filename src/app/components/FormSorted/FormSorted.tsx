8166d3c5bd54aa9740564c648ed86e0d7969d9ef
Aleks <anterebol@gmail.com>
2024-05-06 22:15:39 +0300

    feat: add package mantine/form

--- src/app/components/FormSorted/FormSorted.tsx
+++ src/app/components/FormSorted/FormSorted.tsx
@@ -1,3 +1,11 @@
+import { useForm } from "@mantine/form";
+
 export default function FormSorted() {
-  
+  const form = useForm({
+    mode: 'uncontrolled',
+    initialValues: {
+      name: '',
+      email: '',
+    },
+  });
 }
