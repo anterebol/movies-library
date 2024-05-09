571e00699f76c65e286d1bf49389698eba1d2cf6
Aleks <anterebol@gmail.com>
2024-05-09 13:41:59 +0300

    refactor: add initial constants

--- src/constants/formSortedInitialValues.ts
+++ src/constants/formSortedInitialValues.ts
@@ -1,10 +1,11 @@
+import { KeyAsString } from "@/types/KeyAsString";
+
 const initialValues = {
   genre: '',
   release_year: '',
   rating_from: '',
   rating_to: '', 
-  sort_by: ''
-} as {
-  [key: string]: string
-}
+  sort_by: 'popularity.desc'
+} as KeyAsString;
+
 export default initialValues;
