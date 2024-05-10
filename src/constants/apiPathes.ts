1e553dbc8c6822e29dd1cdaebbda4960bd5f8ccb
Aleks <anterebol@gmail.com>
2024-05-10 21:18:43 +0300

    refactor: add constants for thunks name and api pathes

---
+++ src/constants/apiPathes.ts
@@ -0,0 +1,3 @@
+export const GENRES_PATH = 'genres';
+export const POSTER_CONFIG_PATH = 'poster_config';
+export const SEARCH_MOVIES_PATH = 'search_movies';
---
+++ src/constants/thunksName.ts
@@ -0,0 +1,3 @@
+export const GET_MOVIES = 'getMovies';
+export const GET_POSTERS_CONFIG = 'getPostersConfig';
+export const GET_GENRES = 'getGenres';
