b27838aab680a3fd922ef2cf5c703001bd4b350e
Aleks <anterebol@gmail.com>
2024-05-14 12:26:19 +0300

    reafctor: add movie page for movie card with any information

---
+++ src/app/movie/[id]/page.tsx
@@ -0,0 +1,10 @@
+import { Container } from "@mantine/core";
+
+export default function MoviePage({id}: {id: string}) {
+
+  return (
+    <Container size={'card-container'}>
+
+    </Container>
+  )
+}
--- src/app/movies/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Container } from "@mantine/core"
-
-export default function MoviePage() {
-  return (
-    <Container size={'card-container'}>
-
-    </Container>
-  )
-}
