838438908f75e08b4baed14a86afe125edee6eab
Aleks <anterebol@gmail.com>
2024-05-15 16:28:55 +0300

    feat: add new utils for page link and get current month

--- src/utils/setStringDate.ts
+++ src/utils/setStringDate.ts
@@ -1,7 +1,9 @@
+import { getMonth } from "./getMonth";
+
 export const setStringDate = (date?: string) => {
   if (date) {
     const currentDate = new Date(date || '');
-    const currentDateString = `${currentDate.getMonth()} ${currentDate.getDate()}, ${currentDate.getFullYear()}` 
+    const currentDateString = `${getMonth(currentDate.getMonth())} ${currentDate.getDate()}, ${currentDate.getFullYear()}` 
     return currentDateString;
   }
   return '-/-';
