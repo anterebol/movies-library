37ed25e531ea775c6e533e7cf4f9bd51c56c1253
Aleks <anterebol@gmail.com>
2024-05-18 13:34:53 +0300

    feat: update query string by array genres

--- src/utils/getQueryString.ts
+++ src/utils/getQueryString.ts
@@ -1,9 +1,13 @@
+import { SortType } from "@/constants/formSortedInitialValues";
 import { formSortedKeyToApi } from "@/constants/formSortedKeyToApi";
-import { KeyAsString } from "@/types/KeyAsString";
 
-export const getQueryString = (path: string, page?: string, apiProps?: KeyAsString) => {
+export const getQueryString = (path: string, page?: string, apiProps?: SortType) => {
   const api = Object.entries(apiProps || []).reduce((acc, [key, value]) => {
-    return acc + (value ? `&${formSortedKeyToApi[key]}=${value}` : '');
+    if (!Array.isArray(value)) {
+      return acc + (value ? `&${formSortedKeyToApi[key]}=${value}` : '');
+    } else {
+      return acc + (value.length ? `&${formSortedKeyToApi[key]}=${value.join('%2C')}` : '');
+    }
   }, '');
   return `/${path}/${api}${page ? `&page=${page}` : ''}`;
 }
